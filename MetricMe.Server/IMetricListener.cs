3e105c7d383520b0f60989aaa8891b6d8bc85a42
GrahamThomas <graymant@gmail.com>
2014-03-26 23:48:14 +0100

    Beginning of a plan.

---
+++ MetricMe.Server/IMetricListener.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MetricMe.Server
+{
+    public interface IMetricListener
+    {
+        IObservable<string> Metrics { get; }
+    }
+}
---
+++ MetricMe.Server/UdpMetricListener.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Castle.Core.Logging;
+
+namespace MetricMe.Server
+{
+    public class UdpMetricListener : IMetricListener, IDisposable
+    {
+        private readonly UdpClient socket;
+
+        private ILogger log;
+
+        public UdpMetricListener(int udplistenerPort)
+        {
+            socket = new UdpClient(udplistenerPort);
+        }
+
+        public ILogger Log
+        {
+            get
+            {
+                return log ?? (log = new NullLogger());
+            }
+            set
+            {
+                log = value;
+            }
+        }
+
+        public IObservable<string> Metrics
+        {
+            get
+            {
+                return
+                    Observable.FromAsync(socket.ReceiveAsync)
+                        .Retry()
+                        .Repeat()
+                        .Finally(OnSubscriptionFinished)
+                        .Select(Convert);
+            }
+        }
+
+        private string Convert(UdpReceiveResult result)
+        {
+            return Encoding.ASCII.GetString(result.Buffer);
+        }
+
+        private void OnSubscriptionFinished()
+        {
+            Log.Debug("Udp listener subscription finished.");
+        }
+
+        public void Dispose()
+        {
+            socket.Close();
+        }
+    }
+}
